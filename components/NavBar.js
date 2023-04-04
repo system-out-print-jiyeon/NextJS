e21e0effe8601199d8dbd3ee0bdad01f40b35209
ljy <jylee@lots-on.co.kr>
2023-04-04 16:34:34 +0900

    module을 사용하여 css변경

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,33 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+import styles from "./NavBar.module.css";
+
 export default function NavBar() {
+    const router = useRouter();
+    console.log(router);
     return (
         <nav>
-            {/* <a/> (anchor)사용하면 브라우저가 다른페이지로 보내기 위해 전체페이지를 새로고침하기때문에 느려짐, 사용권장안함 */}
-            <a href="/">Home</a>
-            <a href="/about">About</a>
+            {/* <a/> (anchor)사용하면 브라우저가 다른페이지로 보내기 위해 전체페이지를 새로고침하기때문에 느려짐, 사용권장안함 
+             next/link에서 제공하는 link사용 */}
+            <Link href="/" legacyBehavior>
+                <a
+                className={`${styles.link} ${
+                    router.pathname === "/" ? styles.active : ""
+                }`}
+                >
+                Home
+                </a>
+            </Link>
+            <Link href="/about" legacyBehavior>
+                <a
+                className={[
+                    styles.link,
+                    router.pathname === "/about" ? styles.active : "",
+                ].join(" ")}
+                >
+                About
+                </a>
+            </Link>
         </nav>
     );
 }
