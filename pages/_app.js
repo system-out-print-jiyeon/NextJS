94a8a026b365e0783b96373f9a2887ee2e8f840c
ljy <jylee@lots-on.co.kr>
2023-04-05 10:10:38 +0900

    공통 Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,12 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 // 페이지나 컴포넌트 안에 css를 import하고싶다면, 그건 module이여야한다.
 // 하지만 커스텀App컴포넌트가 있는 곳에서라면 모든 global styles 임포트 가능
-import "../styles/globals.css";
+// import "../styles/globals.css";
 
 export default function App({Component, pageProps}) {
     return (
-        <>
-            <NavBar/>
+        <Layout>
             <Component {...pageProps}/>
-            <style jsx global>{`
-                a {
-                    color: white;
-                }
-            `}</style>
-        </>
+        </Layout>
     )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,10 @@
+import Head from "next/head";
+import Seo from "../components/Seo";
+
 export default function Potato() {
     return (
         <div>
+            <Seo title={"About"}/>
             <h1>About</h1>
         </div>
     );
